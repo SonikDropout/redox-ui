84c272aa894cafb7b47687ba7a1ba03e159bb184
Ефимов Александр <a.efimov@inenergy.ru>
2020-07-02 13:50:13 +0300

    add version to ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
   import Chart from './layouts/Chart';
   import Settings from './layouts/Settings';
+  import Version from './atoms/Version';
 </script>
 
   <Settings />
   <Chart />
+  <Version />
 
