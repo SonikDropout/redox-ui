9a12d889f805943bd53a726ff401eb45db556e1e
Ефимов Александр <a.efimov@inenergy.ru>
2020-07-16 11:08:10 +0300

    update installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 # MAIN APP INSTALLATION
-npm i
-npm run build
+npm i &&
+npm run build &&
 
 # MAIN APP AUTOSTART
 sudo mkdir /usr/share/redox
 sudo cp -rf dist/linux-armv7l-unpacked/** /usr/share/redox/
 echo '/usr/share/redox/redox-ui' > ~/.xinitrc
 chmod +x ~/.xinitrc
+
+while read p; do sudo systemctl disable "$p"; done < unused-packages.list
